c4d3fd989dc3e379dba1d7d685c8c84ae9ea6daf
wangying <wangying8518238@163.com>
2024-10-16 17:07:59 +0800

    .

--- examples/apps/colorapp/src/colorteller/main.go
+++ examples/apps/colorapp/src/colorteller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 
@@ -41,15 +42,32 @@ func getStage() string {
 }
 
 type colorHandler struct{}
+
 func (h *colorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Println("color requested, responding with", getColor())
 	fmt.Fprint(writer, getColor())
 }
 
 type pingHandler struct{}
+
 func (h *pingHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Println("ping requested, reponding with HTTP 200")
 	writer.WriteHeader(http.StatusOK)
+}
+
+type retryTestHandler struct{}
+
+func (h *retryTestHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	a := rand.Intn(100)
+	if a < 50 {
+		writer.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(writer, "调用接口失败，这是随机返回失败")
+		return
+	}
+
+	// ���用接口成功，这是��机返回成功
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprintf(writer, "调用接口成功，这是随机返回成功")
 }
 
 func main() {
@@ -57,5 +75,6 @@ func main() {
 	xraySegmentNamer := xray.NewFixedSegmentNamer(fmt.Sprintf("%s-colorteller-%s", getStage(), getColor()))
 	http.Handle("/", xray.Handler(xraySegmentNamer, &colorHandler{}))
 	http.Handle("/ping", xray.Handler(xraySegmentNamer, &pingHandler{}))
+	http.Handle("/retryTest", xray.Handler(xraySegmentNamer, &retryTestHandler{}))
 	http.ListenAndServe(":"+getServerPort(), nil)
 }
